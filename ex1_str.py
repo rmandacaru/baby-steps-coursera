919a2bb9c7ba983d57041097a3024a4f330c64ee
rmandacaru <47232671+rmandacaru@users.noreply.github.com>
2019-05-03 09:24:12 -0300

    Update ex1_str.py

--- ex1_str.py
+++ ex1_str.py
@@ -1,26 +1,9 @@
-#Escreva a função maiusculas(frase) que recebe uma frase (uma string)
-#como parâmetro e devolve uma string com as letras maiúsculas que existem nesta frase,
-#na ordem em que elas aparecem.
-
-#Para resolver este exercício, pode ser útil verificar uma tabela ASCII,
-#que contém os valores de cada caractere. Ver https://pt.wikipedia.org/wiki/ASCII
-
-#Dica: Os valores apresentados na tabela são os mesmos devolvidos pela função
-#ord apresentada nas aulas.
-
-#maiusculas('Programamos em python 2?')
-# deve devolver 'P'
-
-#maiusculas('Programamos em Python 3.')
-# deve devolver 'PP'
-
-#maiusculas('PrOgRaMaMoS em python!')
-# deve devolver 'PORMMS'
+#esta função ecebe uma frase como parâmetro e devolve uma string com as letras maiúsculas que existem na frase em ordem que aparecem. 
 
 def maiusculas(frases):
     x=''
     for i in frases:
-        if ord(i)>= 65 and ord(i)<= 90:
+        if ord(i)>= 65 and ord(i)<= 90: #conforme a tabela ASCII, que define os valores dos caracteres
             x= x+i
     return x 
 
